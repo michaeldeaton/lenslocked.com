a6278cc9c008a19db40f7284ef98a53260e2768d
Michael Eaton <me@michaeleaton.me>
2020-06-15 17:10:24 +0100

    removed testing bits

--- views/view.go
+++ views/view.go
@@ -10,16 +10,13 @@ func NewView(layout string, files ...string) *View {
 	if err != nil {
 		panic(err)
 	}
-	test := "g"
 	return &View{
 		Template: t,
 		Layout:   layout,
-		Test:     test,
 	}
 }
 
 type View struct {
 	Template *template.Template
 	Layout   string
-	Test     string
 }
